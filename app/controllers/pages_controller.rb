cce1f097b52e6d9f42aef7390c1af4196df297dc
Sanket Patel <sanketplus@gmail.com>
2015-07-03 12:30:01 +0530

    added pagination

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
 
 	def index
-		@links = Link.all
+		params[:per_page] ||=2
+		params[:page] ||= 1
+		@links = Link.order('created_at DESC').paginate(:page =>params[:page],:per_page =>params[:per_page]);
 	end
 
 end
